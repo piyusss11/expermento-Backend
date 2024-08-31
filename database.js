b6bb18b906d20bb1cff487e0ad7ff1ff3fa66427
piyusss11 <piyusss11@gmail.com>
2024-08-31 10:10:24 +0530

    used mongodb

--- database.js
+++ database.js
@@ -1,4 +1,41 @@
-const URI =
+const { MongoClient } = require("mongodb");
+const url =
+  "mongodb+srv://piyush:v4siujYTtg5MHArt@cluster0.tnbafga.mongodb.net/";
 
-  "mongodb+srv://piyush:v4siujYTtg5MHArt@cluster0.tnbafga.mongodb.net/"
+const clinet = new MongoClient(url);
 
+const dbName = "UserKaDb";
+
+const data = {
+  name: "dumdum",
+  city: "delhi",
+};
+async function main() {
+  await clinet.connect();
+  console.log("Connection Succesfully to the Server");
+  const db = clinet.db(dbName);
+  const collection = db.collection("User");
+
+  // const insertResult = await collection.insertMany([data]);
+  // console.log("Inserted documents =>", insertResult);
+
+  const updateResult = await collection.updateOne(
+    { name: "mogambo" },
+    { $set: { name: "John Cena" } }
+  );
+  // console.log("Updated documents =>", updateResult);
+
+  // const findResult = await collection.find({}).toArray();
+  // console.log("Found documents =>", findResult);
+
+  const findResult = await collection.find({name:"John Cena"}).count();
+  console.log("Found documents =>", findResult);
+
+
+  return "done.";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => clinet.close());
